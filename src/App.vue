1746551c8cd8f19dda418e966758326aec2e5929
Maria <60785083+sereginamaria@users.noreply.github.com>
2021-01-19 19:58:04 +0300

    Upload files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,139 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-sm-6 mx-auto">
+        <form @submit.prevent="registrationUser">
+
+          <div class="form-group">
+            <label for="name">Имя</label>
+            <input  @blur="$v.formReg.name.$touch()"
+                    :class="{'is-invalid': $v.formReg.name.$error}"
+                   v-model="formReg.name" type="text" class="form-control" id="name">
+
+            <div v-if="!$v.formReg.name.required" class="invalid-feedback">
+              {{reqText}}
+            </div>
+
+          </div>
+
+          <div class="form-group">
+            <label for="surname">Фамилия</label>
+            <input v-model="formReg.surname" type="text" class="form-control" id="surname">
+          </div>
+
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input @blur="$v.formReg.email.$touch()"
+                   :class="{'is-invalid': $v.formReg.email.$error}"
+                    v-model="formReg.email" type="email" class="form-control" id="email">
+
+            <div v-if="!$v.formReg.email.required" class="invalid-feedback">
+              {{reqText}}
+            </div>
+
+            <div v-if="!$v.formReg.email.email" class="invalid-feedback">
+              Поле должно быть email адресом
+            </div>
+
+          </div>
+
+          <div class="form-group">
+            <label for="password">Пароль</label>
+            <input @blur="$v.formReg.password.$touch()"
+                   :class="{'is-invalid': $v.formReg.password.$error}"
+                    v-model="formReg.password" type="password" class="form-control" id="password" aria-describedby="check">
+            <div v-if="!$v.formReg.password.required" class="invalid-feedback">
+              {{reqText}}
+            </div>
+            <div v-if="!$v.formReg.password.minLength" class="invalid-feedback">
+              Пароль должен содержать не менее 6 символов
+            </div>
+
+            </div>
+
+          <div class="form-group">
+            <label for="passwordConfirm">Повторите пароль</label>
+            <input @blur="$v.formReg.passwordConfirm.$touch()"
+                   :class="{'is-invalid': $v.formReg.passwordConfirm.$error}"
+                    v-model="formReg.passwordConfirm" type="password" class="form-control" id="passwordConfirm">
+
+            <div v-if="!$v.formReg.passwordConfirm.required" class="invalid-feedback">
+              {{reqText}}
+            </div>
+
+            <div v-if="!$v.formReg.passwordConfirm.sameAsPassword && $v.formReg.passwordConfirm.required" class="invalid-feedback">
+              Пароли не совпадают
+            </div>
+
+          </div>
+
+          <button :disabled="disabledButtonSubmit"
+                  type="submit" class="btn btn-primary">Зарегистрироваться</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { required, email, minLength, sameAs} from 'vuelidate/lib/validators'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      reqText: 'Поле обязательно для заполнения',
+      formReg: {
+        name: "",
+        surname: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      }
+    }
+  },
+  computed: {
+    disabledButtonSubmit() {
+      return this.$v.formReg.name.$invalid ||
+              this.$v.formReg.email.$invalid ||
+              this.$v.formReg.password.$invalid ||
+              this.$v.formReg.passwordConfirm.$invalid
+    }
+  },
+  methods:{
+    registrationUser() {
+      console.log('Регистрация прошла успешно!')
+
+      for (let input in this.formReg) {
+        this.formReg[input] = ''
+
+      this.$v.$reset()
+      }
+    }
+  },
+  validations: {
+    formReg: {
+      name: {
+        required
+      },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      passwordConfirm: {
+        required,
+        sameAsPassword: sameAs('password')
+
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
